a97d9bd0ea9aeea00a7107239a420a30573785cf
Monika Januszek <monika.januszek.mail@gmail.com>
2024-05-20 17:54:50 +0200

    working on contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contact.scss";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const [message, setMessage] = useState("");
+  const [isSend, setIsSend] = useState(false);
+  const [error, setError] = useState("");
+
+  function handleSubmitMessage(e) {
+    e.preventDefault();
+    console.log("message", message);
+  }
+
   return (
     <>
       <Navigation />
-      <div className="contact-page"></div>
+      <div className="contact-page">
+        <form className="contact-form" onSubmit={handleSubmitMessage}>
+          <input
+            className="name-input"
+            type="text"
+            required
+            placeholder="Name"
+          />
+          <textarea
+            placeholder="Your message..."
+            className="textarea"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+          <button className="form-btn" type="submit">
+            Wyślij
+          </button>
+        </form>
+      </div>
       <Footer />
     </>
   );
